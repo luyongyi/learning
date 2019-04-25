94718b974cf46d9f180b7dcd4f912907bbdfcb5d
Mylu <lu452803964@qq.com>
2019-04-25 17:17:40 +0800

    第一个类，tips：默认参数只能在声明或定义设定，不然会报错

---
+++ CLASS_Learn/base_class.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+class sample{
+    public:
+    double length;
+    double breadth;
+    double height;
+    void print();
+    sample(int i,int b,int c);
+};
+void sample::print()
+{
+    cout<<length*breadth*height<<endl;
+}
+sample::sample(int i=2,int b=2,int c=2)
+{
+    length=i;
+    breadth=b;
+    height=c;
+    cout<<"create success"<<endl;
+}
+
+int main()
+{
+    sample A;
+    A.print();
+}
